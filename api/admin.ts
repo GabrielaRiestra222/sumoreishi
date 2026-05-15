04c17c7784f72cb83c1aefbfd30dcababe65e478
GabrielaRiestra222 <gabriela.riestra.lucas@gmail.com>
2026-05-15 20:05:59 +0200

    verceldeploy

--- api/admin.ts
+++ api/admin.ts
@@ -16,55 +16,55 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
   // Login
   if (isLoginRequest) {
-    const { default: h } = await import('./admin/login.js');
+    const { default: h } = await import('./_admin/login.js');
     return h(req, res);
   }
 
   // Orders list
   if (path === '/orders' || path === '/orders/') {
-    const { default: h } = await import('./admin/orders.js');
+    const { default: h } = await import('./_admin/orders.js');
     return h(req, res);
   }
 
   // Order detail /orders/:id
   if (path.match(/^\/orders\/[^/]+$/)) {
-    const { default: h } = await import('./admin/orders/[id].js');
+    const { default: h } = await import('./_admin/orders/[id].js');
     return h(req, res);
   }
 
   // Export CSV
   if (path === '/export' || path === '/export/') {
-    const { default: h } = await import('./admin/export.js');
+    const { default: h } = await import('./_admin/export.js');
     return h(req, res);
   }
 
   // Contacts
   if (path === '/contacts' || path === '/contacts/') {
-    const { default: h } = await import('./admin/contacts.js');
+    const { default: h } = await import('./_admin/contacts.js');
     return h(req, res);
   }
 
   // Blog list/create/update/delete
   if (path === '/blog' || path === '/blog/') {
-    const { default: h } = await import('./admin/blog.js');
+    const { default: h } = await import('./_admin/blog.js');
     return h(req, res);
   }
 
   // Blog post detail /blog/:id
   if (path.match(/^\/blog\/[^/]+$/)) {
-    const { default: h } = await import('./admin/blog/[id].js');
+    const { default: h } = await import('./_admin/blog/[id].js');
     return h(req, res);
   }
 
   // Products
   if (path === '/products' || path === '/products/') {
-    const { default: h } = await import('./admin/products.js');
+    const { default: h } = await import('./_admin/products.js');
     return h(req, res);
   }
 
   // Shipping rates
   if (path === '/shipping-rates' || path === '/shipping-rates/') {
-    const { default: h } = await import('./admin/shipping-rates.js');
+    const { default: h } = await import('./_admin/shipping-rates.js');
     return h(req, res);
   }
 
